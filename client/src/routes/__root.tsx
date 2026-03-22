e5c2f2ff1d9e88e74791e59e9eb50c24f8fcd5f6
Jan Phillip Dacallos <72440962+jpdevdotcom@users.noreply.github.com>
2026-03-22 20:29:05 +0800

    feat: add AnalyticsTracker component for enhanced page view tracking (#103)

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import '../styles.css'
+import { AnalyticsTracker } from '@/components/AnalyticsTracker'
 import { Header } from '@/components/Header'
 import { NotificationPermissionPrompt } from '@/components/NotificationPermissionPrompt'
 import { SiteStructuredData } from '@/components/SiteStructuredData'
@@ -15,6 +16,7 @@ function RootComponent() {
   return (
     <>
       <SiteStructuredData />
+      <AnalyticsTracker />
       <a
         href="#main-content"
         className="sr-only focus:not-sr-only focus:absolute focus:left-4 focus:top-4 focus:z-50 focus:rounded-md focus:bg-white focus:px-3 focus:py-2 focus:text-sm focus:font-medium focus:text-blue-700 focus:shadow"
