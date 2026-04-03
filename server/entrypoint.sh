f6b9d45a50f661add9419ba1e14386a10cf9b6ba
Philip <jphillipdacallos@gmail.com>
2026-04-03 11:19:40 +0800

    fix: add makemigrations command to entrypoint script

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+python manage.py makemigrations
 python manage.py migrate
 python manage.py createcachetable --verbosity 0 || true
 python manage.py collectstatic --noinput
